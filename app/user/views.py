226349e05815d6996334b04db5fd1b3a8eaff84e
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-08 15:57:21 +0200

    user create view

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
 
+from user.serializers import UserSerializer
 # Create your views here.
+
+
+class CreateUserView(generics.CreateView):
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
+    

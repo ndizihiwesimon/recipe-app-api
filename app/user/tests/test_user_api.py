278f6a74c1a54fc4e2b5524303d726e728f44b71
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-08 18:54:24 +0200

    Created test_create_token_missing_field

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -90,4 +90,9 @@ class PublicUserApiTest(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_create_token_missing_field(self):
+        """Test that email and password fields are required"""
+        res = self.client.post(TOKEN_URL, {"email": "test@nomisodev.com", "password": ""})
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
     

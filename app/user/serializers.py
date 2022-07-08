f6e5677ff5379ce849850deddd5ec4aa4c82ee8f
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-08 17:16:01 +0200

    resolved linting

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for User objects"""
 
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -19,7 +19,6 @@ class PublicUserApiTest(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-
     def test_create_valid_user_success(self):
         """Test creating user with valid payload is successful"""
         payload = {
@@ -36,7 +35,10 @@ class PublicUserApiTest(TestCase):
 
     def test_user_exist(self):
         """Test creating a user that already exists fails"""
-        payload = {"email": "test@nomisodev.com", "password": "testPass"}
+        payload = {
+            "email": "test@nomisodev.com",
+            "password": "testPass"
+            }
         create_user(**payload)
 
         res = self.client.post(CREATE_USER_URL, payload)
@@ -49,5 +51,6 @@ class PublicUserApiTest(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        user_exists = get_user_model().objects.filter(
+                                email=payload["email"]).exists()
         self.assertFalse(user_exists)

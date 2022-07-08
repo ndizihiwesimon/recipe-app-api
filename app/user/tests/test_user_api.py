c8ab17d62234d1979b3f8f5d7645b05a7416ea3c
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-08 18:46:04 +0200

    created test_create_token_for_user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse("user:create")
+TOKEN_URL = reverse("user:token")
 
 
 def create_user(**params):
@@ -59,3 +60,15 @@ class PublicUserApiTest(TestCase):
         user_exists = get_user_model().objects.filter(
                                 email=payload["email"]).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test creating a token for a user"""
+        payload = {
+            "email": "test@nomisodev.com",
+            "password": "testPass",
+        }
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        

086ea3ccc1e2a23ad417bd78364a71153d6352ed
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-11 18:03:09 +0200

    changed email to lowercase

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,7 +18,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
-        email = 'test@TEST.COM'
+        email = 'test@test.com'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())

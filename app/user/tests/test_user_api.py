645e8b9341749d3f00aa6241fa6dda0d73aa4225
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-12 18:23:18 +0200

    DONE test_update_user_profile

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -136,4 +136,11 @@ class PrivateUserApiTests(TestCase):
 
     def test_update_user_profile(self):
         """Test that updating the user profile for authenticated user"""
-        payload = {'name': 'John', 'email': 'john@example.com', 'password': 'password', }        
+        payload = {'name': 'New Nomiso', 'email': 'new@nomisodev.com', 'password': 'NewPassword'}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)

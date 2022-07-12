47092a83cb3161c8cee15d454bfcf6486b7200be
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2022-07-12 17:59:17 +0200

    test_post_me_not_allowed

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -123,3 +123,11 @@ class PrivateUserApiTests(TestCase):
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST request is not allowed on the me url"""
+        
